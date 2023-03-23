677c9d54f518205c34187fe9f2a6d96937e59b60
小猪你的佩琪呐 <an429075824@qq.com>
2023-03-23 15:26:15 +0800

    Add files via upload

---
+++ zte weixin/config_utils.py
@@ -0,0 +1,32 @@
+import json
+
+def get_config(filename):
+    f = open(filename, 'r')
+    content = f.read()
+    f.close()
+    conf = json.loads(content)
+    return fill_default_config(conf)
+
+def set_config_default_value(config, key, value):
+    try:
+        config[key]
+    except KeyError:
+        config[key] = value
+
+def fill_default_config(config):
+    set_config_default_value(config, 'qywx_host', 'qyapi.weixin.qq.com')
+    set_config_default_value(config, 'corp_id', '')
+    set_config_default_value(config, 'corp_secret', '')
+    set_config_default_value(config, 'agent_id', '')
+    set_config_default_value(config, 'qywx_user', '')
+    set_config_default_value(config, 'trust_command_from', [])
+    set_config_default_value(config, 'modems', [])
+    return config
+
+#def fill_default_config(config):
+    #set_config_default_value(config, 'telegram_host', 'api.telegram.org')
+    #set_config_default_value(config, 'bot_token', '')
+    #set_config_default_value(config, 'telegram_chat_id', '')
+    #set_config_default_value(config, 'trust_command_from', [])
+    #set_config_default_value(config, 'modems', [])
+    #return config
