7fa5492d3d2f49a0523605283280b6d4975a2450
小猪你的佩琪呐 <an429075824@qq.com>
2023-03-29 15:25:30 +0800

    Delete zte weixin directory

--- zte weixin/__main__.py
+++
@@ -1,8 +0,0 @@
-import sms_forwarder
-import config_utils
-
-if __name__ == '__main__':
-    conf = config_utils.get_config('config.json')
-    forwarder = sms_forwarder.SmsForwarder(conf)
-    forwarder.start()
-    #forwarder.do_loop_get_sms_task()
